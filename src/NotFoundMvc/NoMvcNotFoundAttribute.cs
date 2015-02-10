0aa18dbec874d92120454cf07dc60dd6d6961ca8
Bojan Rajkovic <brajkovic@xamarin.com>
2015-02-10 13:21:58 -0500

    D'oh! Add missing file.

---
+++ src/NotFoundMvc/NoMvcNotFoundAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NotFoundMvc
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    class NoMvcNotFoundAttribute : Attribute
+    {
+    }
+}
